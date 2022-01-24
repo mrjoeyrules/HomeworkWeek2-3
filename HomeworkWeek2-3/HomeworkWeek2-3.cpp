b472b0713c3069a942f26503d8bc929c1e9410f1
Joseph Cuesta Acevedo <N1001723@my.ntu.ac.uk>
2022-01-24 22:40:42 +0000

    Completed homework 3

--- HomeworkWeek2-3/HomeworkWeek2-3.cpp
+++ HomeworkWeek2-3/HomeworkWeek2-3.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+double doubleValidation(string inputMessage)
+{
+    bool loopControl = true;
+    string rawValue;
+    while (loopControl)
+    {
+        cout << inputMessage << endl;
+        cin >> rawValue;
+        try
+        {
+            double convertedValue = stod(rawValue);
+            loopControl = false;
+            return convertedValue;
+        }
+        catch (const std::exception&)
+        {
+            cout << "Value " << rawValue << " couldn't be converted to Double, Try again" << endl;
+        }
+    }
+}
+
 
 int main()
 {
-    cout << "Hello World!\n";
+    double dollar = 1;
+    double pound = 0;
+    double conversionRate = doubleValidation("Enter the conversion rate for pounds per dollar: \n");
+    while (true)
+    {
+        pound = 0;
+        pound = dollar * conversionRate;
+        cout << "Dollar Pound" << endl;
+        cout << dollar << "      " << pound << endl;
+        dollar++;
+        if (pound > 10)
+        {
+            break;
+        }
+        else
+        {
+            continue;
+        }
+
+    }
+    
 }
